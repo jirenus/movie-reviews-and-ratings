1908fc42503d1af34505633e1f1f69d00a78325a
ngotruongtuyen <ngotuyen781@gmail.com>
2021-12-24 09:50:55 +0700

    codes: add new feature: user edit profile

--- src/Project/components/user/user.js
+++ src/Project/components/user/user.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require('./userController');
 
-
-/* GET User profile page. */
 router.get('/profile/:id', userController.getProfilePage);
-
+router.get('/editProfile/:id', userController.getEditProfilePage);
+router.post('/editProfile/:id', userController.updateProfile);
 
 module.exports = router;
--- src/Project/components/user/userController.js
+++ src/Project/components/user/userController.js
@@ -6,6 +6,18 @@ class UserController {
         const user = await userService.getOneUser(req.params.id);
         res.render('user/views/profile',{user});
     }
+
+    async getEditProfilePage(req, res, next) {
+        const user = await userService.getOneUser(req.params.id);
+        res.render('user/views/editProfile', {user});
+    }
+
+    async updateProfile(req, res){
+        const id = req.params.id;
+        const result = await userService.updateProfile(id, req.body);
+        const user = await userService.getOneUser(id);
+        res.render('user/views/profile', {result, user});
+    }
 }
 
 module.exports = new UserController();
