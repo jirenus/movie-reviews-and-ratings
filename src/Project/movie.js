e82e506d09e6418b9deed944c8c72861c70360b9
NgoTruongTuyen <ngotuyen781@gmail.com>
2021-12-26 21:41:52 +0700

    codes: fix name of .env file

--- src/Project/movie.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const movieController = require("./movieController");
-/* GET home page. */
-
-
-router.get("/review/:id", movieController.item);
-
-module.exports = router;
